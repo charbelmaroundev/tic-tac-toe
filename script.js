1869c58b61108ad0a5f677448f170870c7e939c2
CharbelMarounDev <charbelmaroun.dev@gmail.com>
2022-09-16 22:14:04 +0300

    detect-player

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 const squareEl = document.querySelectorAll('.square');
+const el1 = document.getElementById('1')
 
 let toggle = true;
 let click = 0;
+
+const playerOne = []
+const playerTwo = []
 
 squareEl.forEach((square) => {
 
     square.addEventListener('click', () => {
         square.style.backgroundRepeat = "no-repeat"
         square.style.backgroundPosition = "center"
-
         if (toggle) {
             square.style.backgroundImage = "url(assets/red.png)"
+            window.onclick = e => {
+                playerOne.push(e.target.id)
+            }
             toggle = !toggle
         } else {
             square.style.backgroundImage = "url(assets/yellow.png)"
+            window.onclick = e => {
+                playerTwo.push(e.target.id)
+            }
             toggle = !toggle
 
         }
-    }
-    );
+    })
 });

1910d155468ebcd00ad841d98a788d518b3a008f
CharbelMarounDev <charbelmaroun.dev@gmail.com>
2022-09-16 21:59:06 +0300

    toggle

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const squareEl = document.querySelectorAll('.square');
 
+let toggle = true;
 let click = 0;
 
 squareEl.forEach((square) => {
@@ -7,6 +8,15 @@ squareEl.forEach((square) => {
     square.addEventListener('click', () => {
         square.style.backgroundRepeat = "no-repeat"
         square.style.backgroundPosition = "center"
+
+        if (toggle) {
+            square.style.backgroundImage = "url(assets/red.png)"
+            toggle = !toggle
+        } else {
+            square.style.backgroundImage = "url(assets/yellow.png)"
+            toggle = !toggle
+
+        }
     }
     );
 });

97430a26e8a36e030756ae5236aa5c63011e04bb
CharbelMarounDev <charbelmaroun.dev@gmail.com>
2022-09-16 22:59:57 +0300

    add-arr-target

--- script.js
+++ script.js
@@ -10,6 +10,31 @@ const playerTwo = []
 squareEl.forEach((square) => {
 
     square.addEventListener('click', () => {
+
+
+        let checker = (arr, target) => target.every(v => arr.includes(v));
+        target = [
+            ['1', '2', '3'],
+            ['4', '5', '6'],
+            ['7', '8', '9'],
+            ['1', '4', '7'],
+            ['2', '5', '8'],
+            ['3', '6', '9'],
+            ['1', '5', '9'],
+            ['7', '5', '3']]
+
+        for (let i = 0; i < 8; i++) {
+            if (checker(playerOne, target[i])) {
+                console.log("PLAYER ONE WIN");
+            };
+        }
+
+        for (let i = 0; i < 8; i++) {
+            if (checker(playerTwo, target[i])) {
+                console.log("PLAYER TWO WIN");
+            };
+        }
+
         square.style.backgroundRepeat = "no-repeat"
         square.style.backgroundPosition = "center"
         if (toggle) {
@@ -24,7 +49,13 @@ squareEl.forEach((square) => {
                 playerTwo.push(e.target.id)
             }
             toggle = !toggle
-
         }
+
+
+
+
+
+
     })
 });
+
